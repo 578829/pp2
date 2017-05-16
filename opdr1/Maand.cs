92a74d68a6455fd6b46576ac591e472620003ff5
Mike hunt <homo@pornhub.com>
2017-05-16 11:47:47 +0200

    fuck

---
+++ opdr1/Maand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdr1
+{
+    enum Maand
+    {
+        Januarie = 1, Februarie, Maart, April, Mei, Juni, Juli, Augustus, September, Oktober, November, December
+    }
+}
---
+++ opdr1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMaanden();
+            VraagMaand();
+            Console.ReadKey();
+        }
+        static void PrintMaanden()
+        {
+            for(Maand M = Maand.Januarie; M <= Maand.December; M++)
+            {
+                Console.WriteLine((int)M+". " + M);
+            }
+        }
+        static Maand VraagMaand()
+        {
+           Console.Write("Geef een maand NR: ");
+           int getal = int.Parse(Console.ReadLine());
+            Maand m;
+
+            if ( getal < 13 && getal > 0)
+            {
+                m = Maand.
+            }
+            else
+            {
+                Console.Write("Geen goede invoer");
+            }
+            return m;
+        }
+    }
+}
